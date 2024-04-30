5f8f34b0ead5ad8efb43fb3a103a27bc08a44e0d
min <min@minui-MacBookPro.local>
2024-04-30 16:12:04 +0900

    env 파일 삭제

--- controllers/ai_img.js
+++ controllers/ai_img.js
@@ -1,34 +1,38 @@
 const axios = require("axios");
 const fs = require("fs");
 const base64Img = require("base64-img");
+const dotenv = require("dotenv");
 
 const User = require("../models/user");
 const Password = require("../models/password");
 const Social_login = require("../models/soical_login");
 const Ai_img = require("../models/ai_img");
 
+dotenv.config();
+
 exports.getTextImg = async (req, res, next) => {
   const imgText = req.body.prompt;
   const imgName = "testimgName";
   const userId = "1";
+  const wuiSetting = {
+    prompt: "masterpiece, (best quality:1.1), 1girl," + imgText,
+    negative_prompt: "nsfw, nude",
+    seed: 1,
+    steps: 20,
+    width: 512,
+    height: 800,
+    cfg_scale: 7,
+    sampler_name: "DPM++ 2M Karras",
+    n_iter: 1,
+    batch_size: 1,
+  };
   if (!req.cookies.authorization) {
     return res.status(406).json({ message: "token doesn't exist" });
   } else {
     try {
       const imgdata = await axios.post(
-        "http://127.0.0.1:7860/sdapi/v1/txt2img",
-        {
-          prompt: "masterpiece, (best quality:1.1), 1girl," + imgText,
-          negative_prompt: "nsfw, nude",
-          seed: 1,
-          steps: 20,
-          width: 512,
-          height: 800,
-          cfg_scale: 7,
-          sampler_name: "DPM++ 2M Karras",
-          n_iter: 1,
-          batch_size: 1,
-        }
+        process.env.WEBUI_ADRESS + "txt2img",
+        wuiSetting
       );
       res.send("data:image/png;base64," + imgdata.data.images[0]);
 
@@ -53,3 +57,11 @@ exports.getTextImg = async (req, res, next) => {
     }
   }
 };
+
+exports.getTagImg = async (res, req, next) => {};
+
+exports.postSaveImg = async (res, req, next) => {};
+
+exports.getLoadImg = async (res, req, next) => {};
+
+exports.deleteImg = async (res, req, next) => {};
